4e9679ba4a21c5fcec3126996fd30f8ec25d99d4
Atul-me <atulkur1999@gmail.com>
2024-02-20 13:21:42 +0530

    2222

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://server-p0fu.onrender.com",
+      "/uploads/": "https://server-p0fu.onrender.com",
     },
   },
 });
