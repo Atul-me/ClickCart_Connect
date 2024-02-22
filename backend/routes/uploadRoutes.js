971043ee2bdfe42495de131025a2156ef44ee76d
Atul-me <atulkur1999@gmail.com>
2024-02-22 10:52:50 +0530

    121

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads/");
+    cb(null, "./frontend/uploads/");
   },
 
   filename: (req, file, cb) => {
