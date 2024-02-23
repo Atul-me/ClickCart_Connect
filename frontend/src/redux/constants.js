34f6c1f908cb7c6607b7285edf47a1fb47602fe7
Atul-me <atulkur1999@gmail.com>
2024-02-23 17:12:38 +0530

    0012

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://server-0hgd.onrender.com";
+export const BASE_URL = "";
 export const USERS_URL = "https://server-0hgd.onrender.com/api/users";
 export const CATEGORY_URL = "https://server-0hgd.onrender.com/api/category";
 export const PRODUCT_URL = "https://server-0hgd.onrender.com/api/products";
